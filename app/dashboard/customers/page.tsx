4cf0c6d2e4b4173b43c8e508f8462fc1b6f8e501
tejaswi0905 <73959528+tejaswi0905@users.noreply.github.com>
2024-04-19 00:25:46 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div>Hello from the cusotmers page.</div>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices() {
+  return <div>Hello from the invoices page</div>;
+}
